f3d5acdb51d0574439a29b2638c0f208f2a4f4fb
P1GUM1 <akinosyoe@gmail.com>
2026-09-19 02:20:01 +0900

    update: マッチング処理及びゲートウェイ処理の記載

--- Assets/Programmer/Script/NetWork/CS_ConnectionGate.cs
+++ Assets/Programmer/Script/NetWork/CS_ConnectionGate.cs
@@ -1,16 +1,44 @@
+using Unity.Netcode;
 using UnityEngine;
+
+// マッチ接続に関するゲートウェイクラス
+// 制作者：　秋野翔太
 
 public class CS_ConnectionGate : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private const int _maxPlayers = 4;
+    [SerializeField] static bool AcceptingConnections = true;
+
+    public static bool IsAcceptingConnections => AcceptingConnections;
+   
+
     void Start()
     {
-        
+        GetComponent<NetworkManager>().ConnectionApprovalCallback = Approve;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Approve(NetworkManager.ConnectionApprovalRequest req,NetworkManager.ConnectionApprovalResponse res)
     {
-        
+        var nm = NetworkManager.Singleton;
+        if (!AcceptingConnections)
+        {
+            res.Approved = false;
+            res.Reason = "ゲームが既に開始されています";
+            return;
+        }
+        if (nm.ConnectedClientsIds.Count >= _maxPlayers)
+        {
+            res.Approved = false;
+            res.Reason = "満員です";
+            return;
+        }
+        res.Approved = true;
+        res.CreatePlayerObject = false;
+    }
+
+    // 接続受付状態を設定するメソッド
+    public void SetAcceptingConnections(bool value)
+    {
+        AcceptingConnections = value;
     }
 }
--- Assets/Programmer/Script/NetWork/CS_Matching.cs
+++ Assets/Programmer/Script/NetWork/CS_Matching.cs
@@ -20,6 +20,9 @@ using UnityEngine.SceneManagement;
  *  切断時
  *  nm.OnClientDisconnectCallback += 関数;
  *  のようにコールバックを登録することで、クライアントの接続・切断を検知できる
+ *  ※　ホストとクライアントではコールバックの呼ばれるタイミングが異なるので注意
+ *  ホスト側では、誰か一人でも接続・切断があった場合に呼ばれる
+ *  クライアント側では、自分が接続・切断した場合に呼ばれる
  */
 // ========================================
 
@@ -36,15 +39,26 @@ public class CS_Matching : MonoBehaviour
     }
 
     // ホストとして部屋を作成する
-   public bool StartAsHost()
+    public bool StartAsHost()
     {
-        var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
-        utp.SetConnectionData("0.0.0.0", 7777, "0.0.0.0");
-        NetworkManager.Singleton.NetworkConfig.ConnectionApproval = true;
-        return NetworkManager.Singleton.StartHost();
+        var nm = NetworkManager.Singleton;
+        nm.GetComponent<UnityTransport>().SetConnectionData("0.0.0.0", 7777, "0.0.0.0");
+
+        if (!nm.StartHost()) return false;
+
+        var status = nm.SceneManager.LoadScene(_matchingWaitingSceneName, LoadSceneMode.Single);
+        if (status != SceneEventProgressStatus.Started)
+        {
+            nm.Shutdown(); // シーン遷移に失敗したら部屋ごと畳む
+            return false;
+        }
+        return true;
     }
 
-    // 参加者として接続する
+    /*
+     * 参加者として部屋に接続する
+     * 戻り値は接続失敗ではなく、接続処理が開始できたかどうかを返す
+     */
     public bool StartAsClient(string ip)
     {
         var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
